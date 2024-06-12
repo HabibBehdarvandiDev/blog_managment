e11b7869d5a9cd26cd262c205410521af14c0441
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-06-13 02:15:10 +0330

    check for username existense

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,19 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
 import { registerSchema } from "./schema";
+import prisma from "@/utils/db";
 
 export async function POST(req: NextRequest) {
   let body;
   try {
     body = await req.json();
   } catch (error) {
-    return NextResponse.json({ error: "بدنه درخواست نمی تواند خالی باشد!" });
+    return NextResponse.json(
+      { error: "بدنه درخواست نمی تواند خالی باشد!" },
+      { status: 400 }
+    );
   }
 
   const validation = registerSchema.safeParse(body);
 
   if (!validation.success) {
-    return NextResponse.json(validation.error.formErrors);
+    return NextResponse.json(validation.error.formErrors, { status: 400 });
   }
 
   const { first_name, last_name, password, username } = validation.data;
+
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+
+  if (isUserExist) {
+    return NextResponse.json(
+      {
+        error:
+          "نام کاربری وارد شده قبلا انتخاب شده است، لطفا نام دیگری انتخاب کنید!",
+      },
+      { status: 400 }
+    );
+  }
+
+  
 }
