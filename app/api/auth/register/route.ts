e03d113b66609a837c67efb9372d153cf0945b4a
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-06-13 02:09:56 +0330

    validate register request body

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { registerSchema } from "./schema";
 
 export async function POST(req: NextRequest) {
   let body;
@@ -8,5 +9,11 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ error: "بدنه درخواست نمی تواند خالی باشد!" });
   }
 
-  
+  const validation = registerSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.formErrors);
+  }
+
+  const { first_name, last_name, password, username } = validation.data;
 }
