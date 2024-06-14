f869f917a2a63f64c41fd0080014452881631373
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-06-15 02:36:45 +0330

    create POST method for creating blog

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -15,3 +15,20 @@ export async function GET(req: NextRequest) {
     );
   }
 }
+
+export async function POST(req: NextRequest) {
+  let body;
+  try {
+    body = await req.json();
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "بدنه درخواست نمی تواند خالی باشد!",
+      },
+      { status: 400 }
+    );
+  }
+
+  
+
+}
