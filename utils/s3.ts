30966926387f389f96e7ea26e772082a9f20d002
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-06-15 06:01:28 +0330

    minor fix

--- utils/s3.ts
+++ utils/s3.ts
@@ -2,6 +2,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client({
   region: process.env.ARVANCLOUD_REGIONS_SIMIN!,
+  endpoint: process.env.BLOG_MANAGMENT_ARVANCLOUD_ENDPOINT!,
   credentials: {
     accessKeyId: process.env.ARAVANCLOUD_ACCESS_KEY_ID!,
     secretAccessKey: process.env.ARAVANCLOUD_SECRET_KEY!,
