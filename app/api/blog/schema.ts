33605d78f723228db74a5709f899cdac43890784
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-06-17 15:58:33 +0330

    make thubmnail optional in api

--- app/api/blog/schema.ts
+++ app/api/blog/schema.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const blogSchema = z.object({
-  title: z.string(),
-  content: z.string(),
-  author_id: z.number(),
-  thumbnail_url: z.string(),
+  title: z.string({ required_error: "نام نوشته نمی تواند خالی باشد." }),
+  content: z.string({ required_error: "محتوای نوشته نمی تواند خالی باشد." }),
+  author_id: z.number({ required_error: "آیدی نویسنده باید ارسال شود." }),
+  thumbnail_url: z.string().optional(),
 });
 
 export const blogSchemaPartial = blogSchema.partial().merge(
